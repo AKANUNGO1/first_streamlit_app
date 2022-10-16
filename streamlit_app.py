49b7fffa5d1fbfa47a2eefb101aa72180c5d40b9
Alisha Kanungo <115931118+AKANUNGO1@users.noreply.github.com>
2022-10-17 01:38:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+Streamlit.text('Omega 3 & Blueberry oatmeal')
+Streamlit.text('Kale, Spinach & Rocket Smoothie')
+Streamlit.text('Hard-Boiled Free-Range Egg')
