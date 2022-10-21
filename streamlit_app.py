3d67eb623ec9157239c6d3647b18e3f1d27b7f30
Alisha Kanungo <115931118+AKANUNGO1@users.noreply.github.com>
2022-10-22 02:18:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 #streamlit.text(fruityvice_response.json())#Just writess the json data onto the screen
 #commented the streamlit.text part to stop the webpage from displaying the json directly 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 #In order to normalise the json data we can take the above json part from response & normalise it using pandas
